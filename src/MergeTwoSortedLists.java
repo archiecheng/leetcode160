9be89e34d64b06881403d6eadb62713977e2bc1c
archiecheng <archiechengice@outlook.com>
2024-11-20 10:46:19 -0800

    leetcode4 - merge two sorted lists

---
+++ src/MergeTwoSortedLists.java
@@ -0,0 +1,49 @@
+public class MergeTwoSortedLists {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        if(list1.val < list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+
+    public static void main(String[] args) {
+        int[] l1 = {1,2,4}, l2 = {1,3,4};
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(4);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+
+        ListNode listNode4 = new ListNode(1);
+        ListNode listNode5 = new ListNode(3);
+        ListNode listNode6 = new ListNode(4);
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        ListNode listNode = mergeTwoLists(listNode1, listNode4);
+        while(listNode.next != null){
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+    }
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
